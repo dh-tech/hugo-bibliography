17f683bb03328cfa0a830cbee48c09d453a35618
TatsatJha <TatsatTarun@gmail.com>
2025-05-16 13:09:20 -0700

    add instructions on configuring the group id for the zotero script

--- fetch-zotero.sh
+++ fetch-zotero.sh
@@ -2,7 +2,9 @@
 
 BIB_FILE="data/bibliography.json"
 
+GROUP_ID="5010351"
+
 mkdir -p data
 
-curl -s "https://api.zotero.org/groups/5010351/items?format=json" \
+curl -s "https://api.zotero.org/groups/$GROUP_ID/items?format=json" \
   | jq '[.[] | .data]' > "$BIB_FILE"
