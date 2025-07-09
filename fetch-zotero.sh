eded41cf2c4e58a8c30639275073d9aec4ba832a
TatsatJha <TatsatTarun@gmail.com>
2025-07-09 14:12:14 -0700

    update fetch to also fetch collections data

--- fetch-zotero.sh
+++ fetch-zotero.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 BIB_FILE="data/bibliography.json"
+COLLECTION_FILE="data/collections.json"
 
 GROUP_ID="5010351"
 
 mkdir -p data
 
-curl -s "https://api.zotero.org/groups/$GROUP_ID/items?format=json" \
+curl -s "https://api.zotero.org/groups/$GROUP_ID/items?limit=50?format=json" \
   | jq '[.[] | .data]' > "$BIB_FILE"
+
+curl -s "https://api.zotero.org/groups/$GROUP_ID/collections?format=json" \
+  | jq '[.[] | .data]' > "$COLLECTION_FILE"
