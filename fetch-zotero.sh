7f8a1fe96f86dc766b840db502b3f0c056c8cf13
TatsatJha <TatsatTarun@gmail.com>
2025-07-16 13:41:00 -0700

    fix bug with repeat entries

--- fetch-zotero.sh
+++ fetch-zotero.sh
@@ -2,20 +2,21 @@
 
 BIB_FILE="data/bibliography.json"
 COLLECTION_FILE="data/collections.json"
-GROUP_ID="5945151"
-LIMIT=100
+GROUP_ID="5010351"
+LIMIT=25
 START_INDEX=0
 
 mkdir -p data
 
 echo "[]" > "$BIB_FILE"
+echo "[]" > "$COLLECTION_FILE"
 
 TOTAL_RESULTS=$(curl -s -I "https://api.zotero.org/groups/$GROUP_ID/items?limit=1" \
   | grep -i "Total-Results:" | awk '{print $2}' | tr -d '\r')
 
 while [ "$START_INDEX" -lt "$TOTAL_RESULTS" ]; do
 
-  curl -s "https://api.zotero.org/groups/$GROUP_ID/items?start="$START_INDEX"?limit=100?format=json" \
+  curl -s "https://api.zotero.org/groups/$GROUP_ID/items?start="$START_INDEX"?limit=$LIMIT?format=json" \
   | jq '[.[] | .data]' > "tmp_items.json"
 
   jq -s '.[0] + .[1]' "$BIB_FILE" tmp_items.json > tmp_merged.json
@@ -32,7 +33,7 @@ TOTAL_RESULTS=$(curl -s -I "https://api.zotero.org/groups/$GROUP_ID/collections?
 
 while [ "$START_INDEX" -lt "$TOTAL_RESULTS" ]; do
 
-  curl -s "https://api.zotero.org/groups/$GROUP_ID/collections?start="$START_INDEX"?limit=100?format=json" \
+  curl -s "https://api.zotero.org/groups/$GROUP_ID/collections?start="$START_INDEX"?limit=$LIMIT?format=json" \
   | jq '[.[] | .data]' > "tmp_collections.json"
 
   jq -s '.[0] + .[1]' "$COLLECTION_FILE" tmp_collections.json > tmp_merged.json
